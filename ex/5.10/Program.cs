0cde3bfe494c3531832af11453dbc3925056cf4c
mukik26 <musaabkkr@gmail.com>
2026-09-09 16:02:27 +0200

    exercise 5.10

---
+++ ex/5.10/Program.cs
@@ -0,0 +1,8 @@
+﻿// declaring and assigning a celcius temperature to a variable
+double tempc = 17.0; // the weather temp at 09/09/2026 15:42
+
+// converting to fahrenheit and storing in a variable
+double tempf = 32 + (9.0 / 5.0 * tempc);
+
+// print both temps
+Console.WriteLine("The temperature is " + tempc + " celcius " + "or " + tempf + " Fahrenheit");
