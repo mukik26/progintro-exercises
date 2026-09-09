b8e0d440934a685f45c78d78b2362a20170ee588
mukik26 <musaabkkr@gmail.com>
2026-09-09 14:39:42 +0200

    exercise 5.1

---
+++ ex/5.1/Program.cs
@@ -0,0 +1,8 @@
+﻿sbyte weathertemp = -2;
+Console.WriteLine("The Weather is " + weathertemp + "°");
+
+byte foodtemp = 200;
+Console.WriteLine("Set the oven to " + foodtemp + "°");
+
+float precisetemp = 20.50F;
+Console.WriteLine("Wait for the soloution to reach " + precisetemp + "°");
