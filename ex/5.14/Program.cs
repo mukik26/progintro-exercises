2c1ce80d04116cb0a27361095ceec6a0611f895c
mukik26 <musaabkkr@gmail.com>
2026-09-11 13:18:45 +0200

    exercise 5-14

---
+++ ex/5.14/Program.cs
@@ -0,0 +1,16 @@
+﻿// 7 hardcoded temperatures
+float monday = 21.5F;
+float tuesday = 23.7F;
+float wednesday = 19.6F;
+float thursday = 22.5F;
+float friday = 25.3F;
+float saturday = 21.7F;
+float sunday = 18.9F;
+
+// Print the variables
+Console.WriteLine(tuesday - monday);
+Console.WriteLine(wednesday - tuesday);
+Console.WriteLine(thursday - wednesday);
+Console.WriteLine(friday - thursday);
+Console.WriteLine(saturday - friday);
+Console.WriteLine(sunday - saturday);
