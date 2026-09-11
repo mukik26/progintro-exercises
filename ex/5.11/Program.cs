55cb3d93fa588a4973f159755036a3beaabd9b3e
mukik26 <musaabkkr@gmail.com>
2026-09-11 13:55:14 +0200

    exercise 5.11

---
+++ ex/5.11/Program.cs
@@ -0,0 +1,10 @@
+﻿// agreed starting point is January 1st, 2000.
+uint timestamp = 213321600; // 6 years and 279 days since the agreed starting point
+
+// calculate the timestamp to years
+uint year = timestamp / (86400*365);
+uint days = (timestamp / 86400) % (365 * year);
+
+// print the timestamp in year and days.
+Console.WriteLine("year: " + year);
+Console.WriteLine("days: " + days);
