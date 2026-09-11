51e4eeaa1620f8591d102f713bfa591535f7d064
mukik26 <musaabkkr@gmail.com>
2026-09-11 14:42:29 +0200

    exercise 5.2

---
+++ ex/5.2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Which types are suitable for representing a month?
+byte month = 0;
+
+// print out month numbers
+Console.WriteLine("January: {0}\nFebruary: {1}\nMarch: {2}\nApril: {3}\nMay: {4}\nJune: {5}\njuly: {6}\nAugust: {7}\nSeptember: {8}\nOctober: {9}\nNovember: {10}\nDecember: {11}\n", ++month, ++month, ++month, ++month, ++month, ++month, ++month, ++month, ++month, ++month, ++month, ++month);
+
+month = 0;
+// written in multiple statements
+Console.WriteLine("January: {0}", ++month);
+Console.WriteLine("February: {0}", ++month);
+Console.WriteLine("March: {0}", ++month);
+Console.WriteLine("April: {0}", ++month);
+Console.WriteLine("May: {0}", ++month);
+Console.WriteLine("June: {0}", ++month);
+Console.WriteLine("July: {0}", ++month);
+Console.WriteLine("August: {0}", ++month);
+Console.WriteLine("September: {0}", ++month);
+Console.WriteLine("October: {0}", ++month);
+Console.WriteLine("November: {0}", ++month);
+Console.WriteLine("December: {0}", ++month);
