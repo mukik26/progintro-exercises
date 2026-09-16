3eb4a003f542865e79c525d187d4b98f8bc5d7c6
mukik26 <musaabkkr@gmail.com>
2026-09-16 12:12:22 +0200

    exercise 7.15

---
+++ ex/7.15/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// the person
+Person person = new Person {
+    alive = true,
+    name = "John Doe",
+    age = 32,
+    height = 180,
+    occupation = "none",
+    favourite_food = "idk",
+};
+
+Console.WriteLine(person.alive);
+
+// a struct that represents a person.
+class Person {
+    public bool alive;
+    public string name;
+    public byte age;
+    public ushort height;
+    public string occupation;
+    public string favourite_food;
+}
