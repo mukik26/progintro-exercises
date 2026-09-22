5b416afde90968f39ee702b41f72f3040a6e915c
mukik26 <musaabkkr@gmail.com>
2026-09-22 20:18:11 +0200

    exercise 8.4

---
+++ ex/7.6/Program.cs
@@ -0,0 +1,8 @@
+﻿int size = 11;
+int[] array = new int[size];
+
+for (int i = 0 ; i < array.Length ; i++)
+{
+    array[i] = 3 * i;
+    Console.Write(array[i]+" ");
+}
---
+++ ex/8.4/Program.cs
@@ -0,0 +1,9 @@
+﻿// declare variables
+int a = 10;
+int b = 50;
+
+int add_int(int a, int b){
+    return a + b;
+}
+
+Console.Write("What is {0} + {1}?\nit is {2}", a, b, add_int(a,b));
